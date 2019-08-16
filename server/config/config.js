2549dec48bcd4d044fad486184419c4c612532ad
kamerroficial <kamerr_oficial@hotmail.com>
2019-08-16 11:26:56 -0500

    ResetServer -Github

--- server/config/config.js
+++ server/config/config.js
@@ -15,6 +15,6 @@ let urldb;
 if(process.env.NODE_ENV === "dev"){
     urldb = 'mongodb://localhost:27017/coffe'
 } else {
-    urldb = 'mongodb+srv://kmradd:Irt6EwKjYCA3M7Pk@cluster0-k2k0d.mongodb.net/coffe'
+    urldb = 'URL DE MONGODDB'
 }
 process.env.URLDB = urldb
